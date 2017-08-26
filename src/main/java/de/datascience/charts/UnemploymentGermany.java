cd656377d33a6843b6d1ca550b4b562e6713fa78
lauraliparulo <laura@war-m>
2017-08-26 22:25:06 +0200

    west and east germany series added

--- src/main/java/de/datascience/charts/UnemploymentGermany.java
+++ src/main/java/de/datascience/charts/UnemploymentGermany.java
@@ -17,35 +17,46 @@ import javafx.stage.Stage;
 
 public class UnemploymentGermany extends Application {
 
-	   @Override
-	    public void start(Stage stage) throws Exception {
-	        stage.setTitle("Index Chart Sample");
-	        final NumberAxis yAxis = new NumberAxis(0, 5000000, 1);
-	        final CategoryAxis xAxis = new CategoryAxis();
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Index Chart Sample");
+		final NumberAxis yAxis = new NumberAxis(0, 5000000, 1);
+		final CategoryAxis xAxis = new CategoryAxis();
 
-	        final LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
-	        yAxis.setLabel("People without job");
-	        xAxis.setLabel("year");
-	        lineChart.setTitle("Unemployment in Germnay");
+		final LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
+		yAxis.setLabel("People without job");
+		xAxis.setLabel("year");
+		lineChart.setTitle("Unemployment in Germnay");
 
-	        XYChart.Series series = new XYChart.Series();
+		XYChart.Series series = new XYChart.Series();
+		XYChart.Series seriesWest = new XYChart.Series();
+		XYChart.Series seriesEast = new XYChart.Series();
+		
+		series.setName("Germany");
+		seriesWest.setName("West Germany");
+		seriesEast.setName("East Germany");
+		
+		try (CSVReader dataReader = new CSVReader(new FileReader("docs/unemployment_germany.csv"))) {
+			String[] nextLine;
+			while ((nextLine = dataReader.readNext()) != null) {
+				String year = String.valueOf(nextLine[0]);
+				int population = Integer.parseInt(nextLine[1]);
+				series.getData().add(new XYChart.Data(year, population));
+				int populationWest = Integer.parseInt(nextLine[2]);
+				;
+				seriesWest.getData().add(new XYChart.Data(year, populationWest));
+				int populationEast = Integer.parseInt(nextLine[3]);
+				seriesEast.getData().add(new XYChart.Data(year, populationEast));
+			}
+		}
 
-	        try (CSVReader dataReader = new CSVReader(new FileReader("docs/unemployment_germany.csv"))) {
-	            String[] nextLine;
-	            while ((nextLine = dataReader.readNext()) != null) {
-	                String year = String.valueOf(nextLine[0]);
-	                int population = Integer.parseInt(nextLine[1]);
-	                series.getData().add(new XYChart.Data(year, population));
-	            }
-	        }
+		lineChart.getData().addAll(series, seriesWest, seriesEast);
+		Scene scene = new Scene(lineChart, 500, 400);
+		stage.setScene(scene);
+		stage.show();
+	}
 
-	        lineChart.getData().addAll(series);
-	        Scene scene = new Scene(lineChart, 500, 400);
-	        stage.setScene(scene);
-	        stage.show();
-	    }
-
-	    public static void main(String[] args) {
-	        launch(args);
-	    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
